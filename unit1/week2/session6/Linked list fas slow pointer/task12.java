5365d49600d62df2ee543d797d8f80d818bdd3ee
vinay <talapanenivinay@gmail.com>
2026-09-24 10:07:17 +0530

    Implement palindrome check for linked list

---
+++ unit1/week2/session6/Linked list fas slow pointer/task12.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+
+        // Find the middle
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse the second half
+        ListNode prev = null;
+        ListNode curr = slow;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        // Compare first half and reversed second half
+        ListNode left = head;
+        ListNode right = prev;
+
+        while (right != null) {
+            if (left.val != right.val) {
+                return false;
+            }
+
+            left = left.next;
+            right = right.next;
+        }
+
+        return true;
+    }
+}
